82bf2731afa44a54f1e8d7ad881010678f28ffd4
Blake03 <blake@edelta.net>
2016-11-05 13:24:51 -0500

    adjusting gallery section for plugin

--- page.php
+++ page.php
@@ -51,6 +51,34 @@ get_header(); ?>
 <!--end-welcome-section
 =============================================================================-->
 
+
+
+<!--start-gallery-section
+=============================================================================-->
+
+<section id="gallery" class="gallery">
+    <div class="gallery__wrapper">
+
+            <?php
+            while ( have_posts() ) : the_post();
+
+                get_template_part( 'template-parts/content', 'page' );
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+
+            endwhile; // End of the loop.
+            ?>
+
+  </div><!--wrapper-->
+</section><!--Gallery-->
+
+<!--end-gallery-section
+=============================================================================-->
+
+
 <!--start-about-section
 =============================================================================-->
 
